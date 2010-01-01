f5cf38d7d73e996bf2fa0a9209523ee6a3924094
Henrik Bengtsson <hb@aroma-project.org>
2010-01-01 01:25:09 +0000

    Version: 0.7.0 [2009-12-31] o Added protected as.data.frame() to GenericDataFileSetList. o Now GenericDataFile(NA, mustExist=FALSE) is a valid object.   Made all methods aware of such missing files. o Now extract(ds, c(1,2,NA,4), onMissing="NA") returns a valid   GenericDataFileSet where missing files are returned as missing   GenericDataFile:s. o Added na.rm=TRUE to all getTags() so that it returns NULL in case   the file is missing. o copyTo() of GenericDataFileSet quietly ignores missing files. o Added Rd help for indexOf() of GenericDataFileSet. o BUG FIX: Changed the default to 'parent=0' for getDefaultFullName()   of GenericDataFileSet to be consistent with the documentation. o BUG FIX: Now GenericDataFile(pathname) throws an error if 'pathname'   is refering to a directory. o BUG FIX: getPath() and getDefaultFullName() of GenericDataFileSet   would return a *logical* instead of *character* value. o BUG FIX: indexOf(ds, names) of GenericDataFileSet would return a   *logical* instead of an *integer* vector of NA:s if none of the   names existed.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.filesets@318 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/FullNameInterface.R
+++ R/FullNameInterface.R
@@ -179,6 +179,8 @@ setMethodS3("getName", "FullNameInterface", function(this, ...) {
 #  \item{collapse}{A @character string used to concatenate the tags. 
 #     If @NULL, the tags are not concatenated.}
 #  \item{...}{Not used.}
+#  \item{na.rm}{If @TRUE and the fullname is @NA, then @NULL is returned,
+#     otherwise (character) @NA is returned.}
 # }
 #
 # \value{
@@ -202,8 +204,18 @@ setMethodS3("getName", "FullNameInterface", function(this, ...) {
 #   @seeclass
 # }
 #*/###########################################################################
-setMethodS3("getTags", "FullNameInterface", function(this, pattern=NULL, collapse=NULL, ..., useCustomTags=TRUE) {
+setMethodS3("getTags", "FullNameInterface", function(this, pattern=NULL, collapse=NULL, ..., na.rm=TRUE, useCustomTags=TRUE) {
   fullname <- getFullName(this, ...);
+
+  # If NA, return NA or NULL?
+  if (is.na(fullname)) {
+    if (na.rm) {
+      return(NULL);
+    } else {
+      naValue <- as.character(NA);
+      return(naValue);
+    }
+  }
 
   # The name is anything before the first comma
   name <- gsub("[,].*$", "", fullname);
@@ -499,6 +511,8 @@ setMethodS3("updateFullName", "FullNameInterface", function(this, ...) {
 
 ############################################################################
 # HISTORY:
+# 2009-10-31
+# o Now getTags(..., na.rm=TRUE) returns NULL in case the file is missing.
 # 2009-10-30
 # o ROBUSTIFICATION: Now translateFullName() of FullNameInterface throws
 #   an exception if some fullnames were translated into NA.
