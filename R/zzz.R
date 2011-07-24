72e9c13f603b2c35152cae2670373853c89bb413
Henrik Bengtsson <hb@aroma-project.org>
2011-07-24 18:35:08 +0000

    Version: 1.1.0 [2011-07-24] o Added a namespace to the package, which will be more or less   a requirement in the next major release of R.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.filesets@594 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/zzz.R
+++ R/zzz.R
@@ -3,10 +3,18 @@
 .conflicts.OK <- TRUE
 
 
-.First.lib <- function(libname, pkgname) {
+## .First.lib <- function(libname, pkgname) {
+.onAttach <- function(libname, pkgname) {
   pkg <- Package(pkgname);
   assign(pkgname, pkg, pos=getPosition(pkg));
 
   packageStartupMessage(getName(pkg), " v", getVersion(pkg), " (", 
     getDate(pkg), ") successfully loaded. See ?", pkgname, " for help.");
 }
+
+
+############################################################################
+# HISTORY: 
+# 2011-07-24
+# o Added a namespace to the package.
+############################################################################
