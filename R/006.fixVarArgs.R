00e9cb8ce04486548d633e02b77ca737ea81f3c2
Henrik Bengtsson <hb@aroma-project.org>
2012-03-07 06:11:21 +0000

    Version: 1.1.5 [2012-03-06] o CRAN POLICY: Removed all internal copies of 'base' functions that   have .Internal() calls.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.filesets@716 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/006.fixVarArgs.R
+++ R/006.fixVarArgs.R
@@ -2,11 +2,19 @@
 # turned into default functions by setMethodS3().
 
 append <- appendVarArgs(append);
-readLines <- appendVarArgs(readLines);
+
+# USED TO DO: readLines <- appendVarArgs(readLines)
+readLines <- function(...) UseMethod("readLines");
+setMethodS3("readLines", "default", function(...) {
+  base::readLines(...);
+})
 
 
 ############################################################################
 # HISTORY:
+# 2012-03-06 [HB]
+# o CRAN POLICY: Removed all internal copies of 'base' functions that
+#   have .Internal() calls. 
 # 2007-02-27 [HB]
 # o BUG FIX: Removed explicit reference to 'base' etc again. The reason is 
 #   that if a previous package already modified, say, write(), to become a 
