13aa73e4a4a4444a6ed9998f8aaee19bcb48bc9d
Henrik Bengtsson <hb@aroma-project.org>
2013-10-01 01:35:43 +0000

    o CLEANUP: Dropped R (<= 2.6.0) patch from digest2(), because package now requires R (>= 2.15.0).
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.filesets@1737 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/digest2.R
+++ R/digest2.R
@@ -1,23 +1,13 @@
 digest2 <- function(object, ..., skip="auto", ascii=FALSE) {
   names <- names(object);
-  if (!is.null(names) && getRversion() <= "2.6.0") {
-    # Patch for R v2.6.0, because it seems like "identical" 'names'
-    # vectors can generate different hashcodes, which I suspect has
-    # to do with the recent suffix-tree implementation of strings
-    # added to R v2.6.0, but also not sure anymore ?!? /HB 2007-08-31
-    rechar <- function(strs) {
-      if (!is.null(strs))
-        strs <- sapply(strs, FUN=function(s) rawToChar(charToRaw(s)));
-      strs;
-    }
-    names <- rechar(names);
-    names(object) <- names;
-  }
   digest::digest(object, ..., skip=skip, ascii=ascii);
 } # digest2()
 
 ############################################################################
 # HISTORY:
+# 2013-09-30
+# o CLEANUP: Dropped R (<= 2.6.0) patch from digest2(), because package
+#   now requires R (>= 2.15.0).
 # 2013-05-22
 # o SPEEDUP: Now digest2() only applies the patch if the object has
 #   names attributes and if running on R v2.6.0 or before.
