0a66749341b4cf39f8cdd8bacda1ef3b3c10ff47
Henrik Bengtsson <hb@aroma-project.org>
2013-10-01 03:27:34 +0000

    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.filesets@1739 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/006.fixVarArgs.R
+++ R/006.fixVarArgs.R
@@ -1,9 +1,7 @@
 # Added '...' to some base functions. These will later be
 # turned into default functions by setMethodS3().
-
 append <- appendVarArgs(append);
 
-# USED TO DO: readLines <- appendVarArgs(readLines)
 readLines <- function(...) UseMethod("readLines");
 setMethodS3("readLines", "default", function(...) {
   base::readLines(...);
@@ -14,11 +12,11 @@ setMethodS3("readLines", "default", function(...) {
 # HISTORY:
 # 2012-03-06 [HB]
 # o CRAN POLICY: Removed all internal copies of 'base' functions that
-#   have .Internal() calls. 
+#   have .Internal() calls.
 # 2007-02-27 [HB]
-# o BUG FIX: Removed explicit reference to 'base' etc again. The reason is 
-#   that if a previous package already modified, say, write(), to become a 
-#   generic function, that was overwritten again when this package was 
+# o BUG FIX: Removed explicit reference to 'base' etc again. The reason is
+#   that if a previous package already modified, say, write(), to become a
+#   generic function, that was overwritten again when this package was
 #   loaded.
 # 2007-02-23 [KS]
 # o Make explicit reference to 'base' - this is safer, in case of colMeans()
