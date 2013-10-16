59a25b15f3dbb38a561636b796fe56d0f0772dd1
Henrik Bengtsson <hb@aroma-project.org>
2013-10-16 16:52:49 +0000

    Version: 2.3.0 [2013-10-16] o Minor cleanups. o Bumped up package dependences.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.filesets@1800 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/zzz.R
+++ R/zzz.R
@@ -1,17 +1,12 @@
-# Allows conflicts. For more information, see library() and
-# conflicts() in [R] base.
-.conflicts.OK <- TRUE
-
-
 .onLoad <- function(libname, pkgname) {
   ns <- getNamespace(pkgname);
   pkg <- Package(pkgname);
   assign(pkgname, pkg, envir=ns);
 }
 
-
 .onAttach <- function(libname, pkgname) {
-  startupMessage(get(pkgname, envir=getNamespace(pkgname)));
+  pkg <- get(pkgname, envir=getNamespace(pkgname));
+  startupMessage(pkg);
 }
 
 
