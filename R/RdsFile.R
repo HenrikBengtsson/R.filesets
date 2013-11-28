28ea4b1f57c2f0b29c09493b6b4de1d8e993ca88
Henrik Bengtsson <hb@aroma-project.org>
2013-11-28 05:16:36 +0000

    Version: 2.3.6 [2013-11-27] o Added a generic loadRDS() available for plain files and RdsFile:s.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.filesets@1855 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/RdsFile.R
+++ R/RdsFile.R
@@ -32,14 +32,59 @@ setConstructorS3("RdsFile", function(...) {
   extend(GenericDataFile(...), "RdsFile");
 })
 
+
 setMethodS3("loadObject", "RdsFile", function(this, ...) {
-  pathname <- getPathname(this);
-  readRDS(pathname, ...);
+  loadRDS(this, ...);
+})
+
+
+###########################################################################/**
+# @RdocGeneric loadRDS
+# @alias loadRDS.default
+# @alias loadRDS.RdsFile
+#
+# @title "Reads data from a RDS file"
+#
+# \description{
+#  @get "title".
+# }
+#
+# \usage{
+#  @usage loadRDS,default
+#  @usage loadRDS,RdsFile
+# }
+#
+# \arguments{
+#   \item{file}{A @character string, a @connection, or an @see "RdsFile"
+#    specifying a RDS file/connection to be read.}
+#   \item{...}{Additional arguments passed to @see "base::readRDS".}
+# }
+#
+# \value{
+#  Returns an R object.
+# }
+#
+# @author
+#
+# \seealso{
+#   @see "base::readRDS".
+# }
+#*/###########################################################################
+setMethodS3("loadRDS", "default", function(file, ...) {
+  readRDS(file, ...);
+})
+
+setMethodS3("loadRDS", "RdsFile", function(file, ...) {
+  pathname <- getPathname(file);
+  loadRDS(pathname, ...);
 })
 
 
 ############################################################################
 # HISTORY:
+# 2013-11-27
+# o Added a default loadRDS(), which is a name alias for base::readRDS().
+#   Also added loadRDS() for RdsFile.
 # 2013-11-20
 # o Created.
 ############################################################################
