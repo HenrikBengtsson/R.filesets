fdc293407dcb36cd2128242caf67085d3a68a6f9
Henrik Bengtsson <hb@aroma-project.org>
2014-01-07 07:50:55 +0000

    Added more tests for copyTo() and linkTo() on GenericDataFile:s.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.filesets@1935 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tests/GenericDataFile.R
+++ tests/GenericDataFile.R
@@ -23,6 +23,7 @@ stopifnot(ok)
 # Copy it again by overwriting exiting output
 dfC <- copyTo(df, path=path, overwrite=TRUE)
 print(dfC)
+stopifnot(getChecksum(dfC) == getChecksum(df))
 
 # Cleanup
 file.remove(getPathname(dfC))
@@ -35,6 +36,17 @@ file.remove(getPathname(dfC))
 path <- tempdir()
 dfL <- linkTo(df, path=path)
 print(dfL)
+stopifnot(getChecksum(dfL) == getChecksum(df))
+
+# Copy file (via link)
+if (packageVersion("R.utils") > "1.29.0") {
+  dfLC <- copyTo(dfL, path=file.path(path, "foo"))
+  stopifnot(getChecksum(dfLC) == getChecksum(df))
+  # Cleanup
+  file.remove(getPathname(dfLC))
+}
+
 
 # Cleanup
 file.remove(getPathname(dfL))
+
