dafd380a45786987d860445a2597189efda9a4a3
Henrik Bengtsson <hb@aroma-project.org>
2014-03-30 22:35:21 +0000

    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.filesets@2056 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tests/GenericDataFileSet.dsApply.R
+++ tests/GenericDataFileSet.dsApply.R
@@ -1,4 +1,7 @@
 library("R.filesets")
+isPackageInstalled <- R.utils::isPackageInstalled
+fullTest <- (Sys.getenv("_R_CHECK_FULL_") != "")
+
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
 # Setting up a file set
@@ -18,25 +21,16 @@ res2 <- dsApply(ds, FUN=getFileSize, .parallel="none")
 print(res2)
 stopifnot(identical(res2, res1))
 
-# - - - - - - - - - - - - - - - - - - - - - - - -
-# Advanced alternatives
-# - - - - - - - - - - - - - - - - - - - - - - - -
-fullTest <- (Sys.getenv("_R_CHECK_FULL_") != "")
-if (fullTest) {
-isPackageInstalled <- R.utils::isPackageInstalled
-
 # Alt 3. (via BatchJobs)
-if (isPackageInstalled("BatchJobs")) {
+if (fullTest && isPackageInstalled("BatchJobs")) {
   res3 <- dsApply(ds, FUN=getFileSize, .parallel="BatchJobs")
   print(res3)
   stopifnot(identical(res3, res1))
 }
 
 # Alt 4. (via BiocParallel + BatchJobs)
-if (isPackageInstalled("BiocParallel") && isPackageInstalled("BatchJobs")) {
+if (fullTest && isPackageInstalled("BiocParallel") && isPackageInstalled("BatchJobs")) {
   res4 <- dsApply(ds, FUN=getFileSize, .parallel="BiocParallel::BatchJobs")
   print(res4)
   stopifnot(identical(res4, res1))
 }
-
-} # if (fullTest)
