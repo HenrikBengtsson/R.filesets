c1dd4edd215be777bf88ef38ff21f3b263b3ed8e
hb <hb@aroma-project.org>
2015-09-05 18:23:43 -0700

    More progress output

--- tests/GenericDataFileSet,dsApply.R
+++ tests/GenericDataFileSet,dsApply.R
@@ -18,10 +18,12 @@ print(ds)
 # Get the size of each file
 # - - - - - - - - - - - - - - - - - - - - - - - -
 # Alt 1.
+message("**** lapply()")
 res1 <- lapply(ds, FUN=getFileSize)
 str(res1)
 
 # Alt 2. (via an internal loop)
+message("**** dsApply(..., .parallel='none')")
 res2 <- dsApply(ds, FUN=getFileSize, .parallel="none")
 str(res2)
 ## FIXME: dsApply() returns "short" names whereas lapply() "full" names
@@ -29,6 +31,7 @@ stopifnot(all.equal(res2, res1, check.attributes=FALSE))
 res1 <- res2 ## FIXME: Workaround trick
 
 # Alt 3a. (via eager futures)
+message("**** dsApply(..., .parallel='future') with plan(eager)")
 library("future")
 plan(eager)
 res3a <- dsApply(ds, FUN=getFileSize, .parallel="future")
@@ -36,6 +39,7 @@ str(res3a)
 stopifnot(all.equal(res3a, res1, check.attributes=FALSE))
 
 # Alt 3b. (via lazy futures)
+message("**** dsApply(..., .parallel='future') with plan(lazy)")
 plan(lazy)
 res3b <- dsApply(ds, FUN=getFileSize, .parallel="future")
 str(res3b)
@@ -43,6 +47,7 @@ stopifnot(all.equal(res3b, res1, check.attributes=FALSE))
 
 # Alt 4. (via BatchJobs)
 if (fullTest && isPackageInstalled("BatchJobs")) {
+  message("**** dsApply(..., .parallel='BatchJobs')")
   res4 <- dsApply(ds, FUN=getFileSize, .parallel="BatchJobs")
   print(res4)
   stopifnot(all.equal(res4, res1))
@@ -50,6 +55,7 @@ if (fullTest && isPackageInstalled("BatchJobs")) {
 
 # Alt 5. (via BiocParallel + BatchJobs)
 if (fullTest && isPackageInstalled("BiocParallel") && isPackageInstalled("BatchJobs")) {
+  message("**** dsApply(..., .parallel='BiocParallel::BatchJobs')")
   res5 <- dsApply(ds, FUN=getFileSize, .parallel="BiocParallel::BatchJobs")
   print(res5)
   stopifnot(all.equal(res5, res1))
