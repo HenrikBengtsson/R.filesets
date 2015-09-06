ed396b26ee9c8d97a148bb86d0495c37af2c4d3c
hb <hb@aroma-project.org>
2015-09-05 18:15:04 -0700

    future::plan(future::lazy) requires future (> 0.7.0)

--- tests/GenericDataFileSet,dsApply.R
+++ tests/GenericDataFileSet,dsApply.R
@@ -29,13 +29,14 @@ stopifnot(all.equal(res2, res1, check.attributes=FALSE))
 res1 <- res2 ## FIXME: Workaround trick
 
 # Alt 3a. (via eager futures)
-future::plan(future::eager)
+library("future")
+plan(eager)
 res3a <- dsApply(ds, FUN=getFileSize, .parallel="future")
 str(res3a)
 stopifnot(all.equal(res3a, res1, check.attributes=FALSE))
 
 # Alt 3b. (via lazy futures)
-future::plan(future::lazy)
+plan(lazy)
 res3b <- dsApply(ds, FUN=getFileSize, .parallel="future")
 str(res3b)
 stopifnot(all.equal(res3b, res1, check.attributes=FALSE))
