87ae55664a6c6f35f659f5bf0e8db1e3505db547
hb <hb@aroma-project.org>
2015-10-09 14:21:24 -0700

    PARALLEL: Tests no longer have to attach 'future'

--- tests/GenericDataFileSet,dsApply.R
+++ tests/GenericDataFileSet,dsApply.R
@@ -32,15 +32,14 @@ res1 <- res2 ## FIXME: Workaround trick
 
 # Alt 3a. (via eager futures)
 message("**** dsApply(..., .parallel='future') with plan(eager)")
-library("future")
-plan(eager)
+future::plan("eager")
 res3a <- dsApply(ds, FUN=getFileSize, .parallel="future")
 str(res3a)
 stopifnot(all.equal(res3a, res1, check.attributes=FALSE))
 
 # Alt 3b. (via lazy futures)
 message("**** dsApply(..., .parallel='future') with plan(lazy)")
-plan(lazy)
+future::plan("lazy")
 res3b <- dsApply(ds, FUN=getFileSize, .parallel="future")
 str(res3b)
 stopifnot(all.equal(res3b, res1, check.attributes=FALSE))
