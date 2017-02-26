8417ef12b2cdb7d8fa1a19cdca805488c9c2eca4
hb <hb@aroma-project.org>
2017-02-26 12:30:44 -0800

    Argument 'colClassPatterns' of readDataFrame() for TabularTextFile is now defunct.  Use 'colClasses' instead.

--- R/TabularTextFile.R
+++ R/TabularTextFile.R
@@ -537,11 +537,9 @@ setMethodS3("getReadArguments", "TabularTextFile", function(this, fileHeader=NUL
   verbose && cat(verbose, "User arguments:");
   verbose && str(verbose, userArgs);
 
-  # Backward compatibility
-  if (is.element("colClassPatterns", names(userArgs))) {
-    .Deprecated(msg="Argument 'colClassPatterns' has been renamed to 'colClasses'. Please update your code accordingly.");
-    colClasses <- userArgs[["colClassPatterns"]];
-    userArgs[["colClassPatterns"]] <- NULL;
+  # DEFUNCT
+  if ("colClassPatterns" %in% names(userArgs)) {
+    .Defunct(msg = "Argument 'colClassPatterns' has been renamed to 'colClasses'. Please update your code accordingly.")
   }
 
 
